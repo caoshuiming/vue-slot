c7032825555376fc5a27f7e1f78f9b162af69a5e
caoshuiming <2639067440@qq.com>
2021-01-20 16:43:32 +0800

    new project

---
+++ src/views/Child.vue
@@ -0,0 +1,62 @@
+<!-- 子组件-->
+<!-- vue slot 是组件的一块html模板 ，这块模板由父组件提供。-->
+<!-- 是子组件暴露的一个父组件传入自定义内容的接口-->
+<!-- slot分为三类 默认插槽 具名插槽 作用域插槽-->
+<!-- 子组件里面插槽用slot标签 来确定渲染位置-->
+<template>
+  <div class="content" style="color: green">
+    <!--  默认插槽-->
+    <header class="text">
+      <!-- slot的后备内容：作为一个插槽设置具体后备（默认）内容是很有用的。-->
+      <!-- 当父组件不添加任何插槽内容时，显示默认值-->
+      <slot>默认值</slot>
+    </header>
+  </div>
+</template>
+
+<!--具名插槽用name属性来表示插槽名字，如果不传为默认插槽-->
+<!--<template>-->
+<!--  <div class="content">-->
+<!--    <main class="text">-->
+<!--      <slot name="main"></slot>-->
+<!--    </main>-->
+<!--    <footer class="text">-->
+<!--      <slot name="footer"></slot>-->
+<!--    </footer>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--作用域插槽在作用域上绑定属性来将子组件的信息传给父组件使用-->
+<!--这些属性挂在父组件的slot-scope接收对象上-->
+<!--作用域插槽 父组件可以访问到子组件的数据 子组件中slot标签上面的特性 称为插槽prop-->
+<!--也可以传function-->
+<!--<template>-->
+<!--  <div class="content">-->
+<!--    <footer class="text">-->
+<!--      <slot name="footer" :user="user" :testClick="testClick"></slot>-->
+<!--    </footer>-->
+<!--  </div>-->
+<!--</template>-->
+
+<script>
+  export default {
+    name: "Child",
+    data(){
+      return{
+        user:{
+          title:'csm title',
+          name:'csm name'
+        }
+      }
+    },
+    methods:{
+      testClick:function(){
+        alert('hello')
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,64 @@
+<!--父组件 默认插槽-->
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <child>
+      <p>哈哈哈哈哈，我就是不出现</p>
+      <template v-slot:default>
+        但如果你定义了 default 之后，其他内容就不会出现了，原理同上，不能重复定义
+      </template>
+    </child>
+    <!--独占默认插槽的缩写-->
+    <child v-slot="slotProps">
+      当只有默认插槽时，此为独占默认插槽的缩写<br>
+      如果组件中有其他具名插槽，这么写会报错<br>
+      slotProps取的是，子组件标签slot上属性数据的集合
+    </child>
   </div>
 </template>
 
+<!--父组件 具名插槽-->
+<!--<template>-->
+<!--  <div class="container">-->
+<!--    <child>-->
+<!--      <template v-slot:main>-->
+<!--        <a href="https://www.baidu.com" target="_blank">百度</a>-->
+<!--      </template>-->
+<!--      <template #footer>-->
+<!--        页脚（v-slot:的缩写#）-->
+<!--      </template>-->
+<!--      <template #footer>-->
+<!--        只占了一个位置，写两个或以上就会以最后一个为准-->
+<!--      </template>-->
+<!--    </child>-->
+<!--  </div>-->
+<!--</template>-->
+
+<!--父组件 作用域插槽-->
+<!--<template>-->
+<!--  <div class="container">-->
+<!--    <child>-->
+<!--      <template #footer="slotProps">-->
+<!--        {{slotProps.user.title}}-->
+<!--        <button @click="slotProps.testClick">点我</button>-->
+<!--      </template>-->
+<!--      <template #footer="{user,testClick}">-->
+<!--        {{user.title}}<br>-->
+<!--        此为解构插槽prop<br>-->
+<!--        &lt;!&ndash; 子组件中的通用方法，可传递给父组件复用&ndash;&gt;-->
+<!--        <button @click="testClick">点我</button>-->
+<!--      </template>-->
+<!--    </child>-->
+<!--  </div>-->
+<!--</template>-->
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
+import child from './Child.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    child
   }
 }
 </script>
